6069ee99c4e6e77ffe6ac554fbb096af59985dff
Edgar Z. Alvarenga <edgar@ymonad.com>
2013-09-09 09:19:20 -0300

    Adding package dependencies in setup.py

--- setup.py
+++ setup.py
@@ -9,4 +9,13 @@ setup(name='formcreator',
       license='BSD 2-Clause',
       packages=['formcreator', 'formcreator.fields', 'formcreator.blocks', 'formcreator.static', 'formcreator.templates'],
       include_package_data=True,
-      zip_safe=False)
+      zip_safe=False,
+      install_requires=[
+          'flask==0.10.1',
+          'flask-login==0.2.7',
+          'flask-sqlalchemy==1.0',
+          'wtforms==1.0.3',
+          'markdown==2.2.1',
+          'ordereddict==1.1',
+      ],
+      )
